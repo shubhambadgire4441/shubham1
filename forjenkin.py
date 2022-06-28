345329896676ff1b5e64597bd2ace64ff5d17843
shubhambadgire4441 <shubhambadgire@gmail.com>
2022-06-28 16:40:52 +0530

    done

---
+++ forjenkin.py
@@ -0,0 +1,10 @@
+a="this project for jenkin understand"
+print(a)
+b="i also push the project on git hub"
+print(b)
+c="i change folder in c"
+print(c)
+d="want to pull this "
+print(d)
+e="url copy"
+print(e)
