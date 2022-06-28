3e32a776385a5c304a053e3f1fd45574695776c4
shubhambadgire4441 <107884663+shubhambadgire4441@users.noreply.github.com>
2022-06-28 18:56:19 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,19 @@
+#this program is for addition of two numbers
+a=12
+b=20
+print(a+b)
+
+#the result of addition of two numbers
+
+FN=int(input("enter your first number"))
+SN=int(input("enter your second number"))
+c=FN+SN
+#formating string output
+
+print("the result of addition of",FN,"and",SN,"=",c)
+
+print("the result of addition of "+str(FN)+" and "+str(SN)+" = "+str(c))
+
+
+
+
